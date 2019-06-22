3be59068d06dba178ae34f0b1b719ddf569a6fdc
gj2531 <2531025786@qq.com>
2019-06-22 11:11:15 +0800

    添加了使用文件系统获取hdfs的四种方式

--- big_data6_17/hdfs_day01/src/test/java/hdfsdemo1/HdfsOpertarStudy.java
+++ big_data6_17/hdfs_day01/src/test/java/hdfsdemo1/HdfsOpertarStudy.java
@@ -1,6 +1,8 @@
 package hdfsdemo1;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.FsUrlStreamHandlerFactory;
 import org.testng.annotations.Test;
 
@@ -9,6 +11,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 public class HdfsOpertarStudy {
@@ -23,5 +27,47 @@ public class HdfsOpertarStudy {
         IOUtils.copy(inputStream,outputStream);
         IOUtils.closeQuietly(inputStream);
         IOUtils.closeQuietly(outputStream);
+    }
+
+    /*
+    * 获取分布式文件系统的第一种方式
+    * */
+    @Test
+    public void getHdfs1() throws IOException {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS","hdfs://node01:8020/");
+        FileSystem fileSystem = FileSystem.get(configuration);
+        System.out.println(fileSystem.toString());
+    }
+
+    /*
+    * 获取分布式文件系统的第二种方式
+    * */
+    @Test
+    public void getHdfs2() throws URISyntaxException, IOException {
+        Configuration configuration = new Configuration();
+
+        FileSystem fileSystem = FileSystem.get(new URI("hdfs://node01:8020/"), configuration);
+        System.out.println(fileSystem.toString());
+    }
+
+    /*
+    * 获取分布式文件系统的第三种方式
+    * */
+    @Test
+    public void getHdfs3() throws IOException {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS","hdfs://node01:8020/");
+        FileSystem fileSystem = FileSystem.newInstance(configuration);
+        System.out.println(fileSystem.toString());
+    }
+
+    /*
+    * 获取分布式文件系统的第四种方式
+    * */
+    @Test
+    public void getHdfs4() throws URISyntaxException, IOException {
+        FileSystem fileSystem = FileSystem.newInstance(new URI("hdfs://node01:8020/"), new Configuration());
+        System.out.println(fileSystem.toString());
     }
 }
