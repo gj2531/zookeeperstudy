9bea24a078f169e11e12cacca6e0c13dc0a0871c
gj2531 <2531025786@qq.com>
2019-06-24 14:53:53 +0800

    本次更新了mapreduce在本地执行是输入的路径

--- big_data6_17/hdfs_day01/src/main/java/mapreducewordcount/JobMain.java
+++ big_data6_17/hdfs_day01/src/main/java/mapreducewordcount/JobMain.java
@@ -16,10 +16,14 @@ public class JobMain extends Configured implements Tool {
     @Override
     public int run(String[] strings) throws Exception {
         Job job = Job.getInstance(super.getConf(), JobMain.class.getSimpleName());
+        //要想打包发送到服务器上执行必须加这句话
         job.setJarByClass(JobMain.class);
         //第一步 读取输入文件解析成key,value对
         job.setInputFormatClass(TextInputFormat.class);
-        TextInputFormat.addInputPath(job,new Path("hdfs://node01:8020/wordcount"));
+        //打包到集群上时的路径
+        //TextInputFormat.addInputPath(job,new Path("hdfs://node01:8020/wordcount"));
+        //本地执行时的路径
+        TextInputFormat.addInputPath(job,new Path("file:///G:\\大数据5期\\hadoop\\Hadoop课程资料\\3、大数据离线第三天\\wordcount\\input"));
         //第二步设置mapper类
         job.setMapperClass(WordCountMap.class);
         job.setMapOutputKeyClass(Text.class);
@@ -31,7 +35,10 @@ public class JobMain extends Configured implements Tool {
         job.setOutputValueClass(LongWritable.class);
         //第八步 设置输出类以及输出路径
         job.setOutputFormatClass(TextOutputFormat.class);
-        TextOutputFormat.setOutputPath(job,new Path("hdfs://node01:8020/wordout"));
+        //打包到集群上时的路径
+        //TextInputFormat.addInputPath(job,new Path("hdfs://node01:8020/wordcount_out"));
+        //本地执行时的路径
+        TextOutputFormat.setOutputPath(job,new Path("file:///G:\\大数据5期\\hadoop\\Hadoop课程资料\\3、大数据离线第三天\\wordcount\\output"));
         boolean b = job.waitForCompletion(true);
         return b?0:1;
     }
