57df301a60a7d42b5bfb665f6e0858a589d09d83
gj2531 <2531025786@qq.com>
2019-06-27 16:36:12 +0800

    在对手机上网流量进行求和的基础上，添加了针对前三位相同的手机号进行分区的案例

---
+++ big_data6_17/mapreduce_day04/src/main/java/phonePartitiondemo/FlowReduce.java
@@ -0,0 +1,29 @@
+package phonePartitiondemo;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class FlowReduce extends Reducer<Text, FlowBean,Text, FlowBean> {
+
+    @Override
+    protected void reduce(Text key, Iterable<FlowBean> values, Context context) throws IOException, InterruptedException {
+        Integer upFlow = 0;
+        Integer downFlow = 0;
+        Integer upCountFlow = 0;
+        Integer downCountFlow = 0;
+        for (FlowBean value : values) {
+            upFlow += value.getUpFlow();
+            downFlow += value.getDownFlow();
+            upCountFlow += value.getUpCountFlow();
+            downCountFlow += value.getDownCountFlow();
+        }
+        FlowBean flowBean = new FlowBean();
+        flowBean.setUpFlow(upFlow);
+        flowBean.setDownFlow(downFlow);
+        flowBean.setUpCountFlow(upCountFlow);
+        flowBean.setDownCountFlow(downCountFlow);
+        context.write(key,flowBean);
+    }
+}
